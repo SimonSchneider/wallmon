4545489dc10839201214901d194308cc8b5be654
SimonSchneider <simon@simonschneider.org>
2020-08-29 17:06:32 +0200

    feat: add debug printing flag to reduce output

--- wallmon.go
+++ wallmon.go
@@ -24,6 +24,12 @@ type config struct {
 	chromeCmd       string
 	restartInterval time.Duration
 	restartDelay    time.Duration
+	debug           bool
+}
+
+func (c config) String() string {
+	return fmt.Sprintf("uri: '%s', restart-interval: %s, restart-delay: %s, chrome-cmd: '%s', data-dir: '%s'",
+		c.uri, c.restartInterval, c.restartDelay, c.chromeCmd, c.dataDir)
 }
 
 func main() {
@@ -39,22 +45,28 @@ func main() {
 		fmt.Printf("unable to initialize data dir '%s': %s\n", cnf.dataDir, err)
 		os.Exit(1)
 	}
+	if cnf.debug {
+		fmt.Printf("running with wallmon with %s\n", cnf)
+	}
 	dirArg := fmt.Sprintf("--user-data-dir=%s", cnf.dataDir)
 	args := []string{"--kiosk", dirArg, cnf.uri}
 	for {
 		timeout, _ := context.WithTimeout(context.Background(), cnf.restartInterval)
-		if err := runContext(timeout, cnf.chromeCmd, args...); err != nil {
+		if err := runContext(timeout, cnf.debug, cnf.chromeCmd, args...); err != nil {
 			fmt.Printf("failed to run command: %s\n", err)
 			os.Exit(1)
 		}
 		time.Sleep(cnf.restartDelay)
+		fmt.Printf("restarting chrome\n")
 	}
 }
 
-func runContext(ctx context.Context, cmdName string, args ...string) error {
+func runContext(ctx context.Context, debug bool, cmdName string, args ...string) error {
 	cmd := exec.CommandContext(ctx, cmdName, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	if debug {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("unable to start command: %w", err)
 	}
@@ -69,7 +81,6 @@ func runContext(ctx context.Context, cmdName string, args ...string) error {
 
 func initializeDataDir(dataDir string) error {
 	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
-		fmt.Printf("data dir '%s' does not exist, creating\n", dataDir)
 		err = os.Mkdir(dataDir, os.ModePerm)
 		if err != nil {
 			return fmt.Errorf("creating dataDir: %w", err)
@@ -81,7 +92,6 @@ func initializeDataDir(dataDir string) error {
 	}
 	defer f.Close()
 	return nil
-
 }
 
 func parseAndValidateFlags() (config, error) {
@@ -91,6 +101,7 @@ func parseAndValidateFlags() (config, error) {
 	flag.DurationVar(&cnf.restartInterval, "restart-interval", 12*time.Hour, fmt.Sprintf("restart interval of chrome (min %s)", minRestartInterval))
 	flag.DurationVar(&cnf.restartDelay, "restart-delay", 1*time.Second, fmt.Sprintf("delay between restarts of chrome (min %s)", minRestartDelay))
 	flag.StringVar(&cnf.chromeCmd, "chrome-cmd", defaultChromeCmdName(), "path to chrome cmd")
+	flag.BoolVar(&cnf.debug, "debug", false, "additional output (mainly redirecting chrome stdout to stdout)")
 	flag.Parse()
 	if cnf.chromeCmd == "" {
 		return config{}, fmt.Errorf("chrome path needs to be specified")
